ff1e848e5c09af76e41db36164438d2c25583e71
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-27 11:39:21 -0400

    Formatted HTML and added search function layout
    this feature is unfinished

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,24 @@
-// ---- js file ----
+$("#select1").on('change',function(){
+    var val = $(this).val();
+    if (val == "NJ"){
+        $("#myselect").html("<option value='test'>NJ: NJ 1</option><option value='test2'>NJ: NJ 2</option>");
+    }else if(val == "AL") {
+        ("#myselect").html("<option value='test'>AL: AL 1</option><option value='test2'>AL: AL 2</option>");
+    } else if (val == "WA") {
+        $("#myselect").html("<option value='test'>WA: WA 1</option><option value='test2'>WA: WA 2</option>");
+    } else if (val == "0") {
+        $("#myselect").html("<option value=''>--select one--</option>");
+    }
+});
+
+
+
+// $(function() {
+//     $("#select1").on('change', function() {
+//         $('#myselect').val("1");
+
+//         // re-initialize material-select
+//         $('#myselect').formSelect();
+//     });
+// });
+
