f9e52f6836b7b02cbc415b93d83ba619e67392d9
Ronnieve23 <ronnieve_romero@yahoo.com>
2021-07-27 12:53:51 -0400

    Search dropdown now working;Pending tweaks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
-$("#select1").on('change',function(){
-    var val = $(this).val();
+$("#select1").change(function(){
+    var val = $(this).val();        
     if (val == "NJ"){
         $("#myselect").html("<option value='test'>NJ: NJ 1</option><option value='test2'>NJ: NJ 2</option>");
     }else if(val == "AL") {
-        ("#myselect").html("<option value='test'>AL: AL 1</option><option value='test2'>AL: AL 2</option>");
+        $("#myselect").html("<option value='test'>AL: AL 1</option><option value='test2'>AL: AL 2</option>");
     } else if (val == "WA") {
         $("#myselect").html("<option value='test'>WA: WA 1</option><option value='test2'>WA: WA 2</option>");
     } else if (val == "0") {
         $("#myselect").html("<option value=''>--select one--</option>");
     }
+    $('#myselect').formSelect()
 });
 
 
