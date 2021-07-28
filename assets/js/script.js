50aa66e503099a501c07b3a27ebca38a4a43a747
miraj <miraj00@gmail.com>
2021-07-28 16:53:54 -0400

    8 states added with park visible upon selection

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,15 @@
 
 /*
 // Code List for National Parks :
+
+//var Nevada = [ {Death Valley National Park, deva}, {Great Basin National Park,   }];
+  var Montana = [ {Glacier National Park, glac}, {Yellowstone National Park, yell}];
+  var Hawaii = [ {Haleakala National Park, hale}, {Hawai’i Volcanoes National Park, havo}];
+  var New_Mexico = [ {Carlsbad Caverns National Park, cave}, {White Sands National Park, whsa}];
+  var Arizona = [ {Grand Canyon National Park, grca}, {Petrified Forest National Park, pefo}, {Saguaro National Park, sagu}];
+  var Utah = [ {Arches National Park, arch}, {Bryce Canyon National Park, brca}, {Canyonlands National Park, cany}, {Capitol Reef National Park, care},
+             {Zion National Park, zion}];   
+
 var Alaska = [{park : "Denali National Park", code: "dena"}, {park: "Gates of the Arctic National Park", code: "gaar"}, 
               {park: "Glacier Bay National Park", code: "glba"}, {park: "Katmai National Park", code: "katm"}, 
             {park: "Kenai Fjords National Park", code: "kefj"}, {park: "Kobuk Valley National Park", code: "kova"}, 
@@ -15,19 +24,6 @@ var California = [{Channel Islands National Park, chis}, {Death Valley National 
              {Lassen Volcanic National Park, lavo}, {Pinnacles National Park, pinn}, {Redwood National Park, redw}, 
              {Sequoia and Kings Canyon National Park, seki}, {Yosemite National Park, yose}];             
 
-var Arizona = [ {Grand Canyon National Park, grca}, {Petrified Forest National Park, pefo}, {Saguaro National Park, sagu}]
-
-var Hawaii = [ {Haleakala National Park, hale}, {Hawai’i Volcanoes National Park, havo}]
-
-var Montana = [ {Glacier National Park, glac}, {Yellowstone National Park, yell}]
- 
-var New_Mexico = [ {Carlsbad Caverns National Park, cave}, {White Sands National Park, whsa}]
-
-var Utah = [ {Arches National Park, arch}, {Bryce Canyon National Park, brca}, {Canyonlands National Park, cany}, {Capitol Reef National Park, care},
-             {Zion National Park, zion}]
-
-//var Nevada = [ {Death Valley National Park, deva}, {Great Basin National Park,   }];
-
 */
   
 
@@ -36,6 +32,11 @@ var optionsList = document.querySelector("#mySelection");
 var Nevada = [ "Death Valley National Park", "Great Basin National Park" ];
 var Montana = [ "Glacier National Park", "Yellowstone National Park"];
 var Hawaii = [ "Haleakala National Park", "Hawai’i Volcanoes National Park"];
+var New_Mexico = ["Carlsbad Caverns National Park", "White Sands National Park"];
+var Arizona = [ "Grand Canyon National Park", "Petrified Forest National Park, pefo", "Saguaro National Park, sagu"];
+var Utah = [ "Arches National Park", "Bryce Canyon National Park", "Canyonlands National Park", "Capitol Reef National Park", "Zion National Park"];
+var Alaska = ["Denali National Park","Gates of the Arctic National Park", "Glacier Bay National Park", "Katmai National Park", "Kenai Fjords National Park", "Kobuk Valley National Park", "Lake Clark National Park", "Wrangell St Elias National Park"];
+var California = ["Channel Islands National Park", "Death Valley National Park", "Joshua Tree National Park", "Lassen Volcanic National Park", "Pinnacles National Park", "Redwood National Park", "Sequoia and Kings Canyon National Park", "Yosemite National Park"];  
 
 function myFunction(event) {
       event.preventDefault();
@@ -56,7 +57,6 @@ if ( checkValue === "NEVADA") {
     var Nevada = [ "Death Valley National Park", "Great Basin National Park" ];    
     var parklistEl = document.createElement("li");
     parklistEl.className = "parksList";
-   
     parklistEl.innerHTML = Nevada[i];
     optionsList.appendChild(parklistEl);
     }
@@ -68,13 +68,21 @@ if ( checkValue === "HAWAII") {
         var Hawaii = [ "Haleakala National Park", "Hawai’i Volcanoes National Park"];  
         var parklistEl = document.createElement("li");
         parklistEl.className = "parksList";
-       
         parklistEl.innerHTML = Hawaii[i];
         optionsList.appendChild(parklistEl);
         }
-
 }
+if ( checkValue === "NEW-MEXICO") { 
 
+    for (let i = 0; i < 2; i++) {
+    
+    var New_Mexico = ["Carlsbad Caverns National Park", "White Sands National Park"];
+    var parklistEl = document.createElement("li");
+    parklistEl.className = "parksList";
+    parklistEl.innerHTML = New_Mexico[i];
+    optionsList.appendChild(parklistEl);
+    }
+}
 if ( checkValue === "MONTANA") { 
 
     for (let i = 0; i < 2; i++) {
@@ -82,16 +90,56 @@ if ( checkValue === "MONTANA") {
     var Montana = [ "Glacier National Park", "Yellowstone National Park"];  
     var parklistEl = document.createElement("li");
     parklistEl.className = "parksList";
-   
     parklistEl.innerHTML = Montana[i];
     optionsList.appendChild(parklistEl);
     }
+}
+if ( checkValue === "ARIZONA") { 
 
+    for (let i = 0; i < 3; i++) {
+
+    var Arizona = [ "Grand Canyon National Park", "Petrified Forest National Park, pefo", "Saguaro National Park, sagu"];
+    var parklistEl = document.createElement("li");
+    parklistEl.className = "parksList";
+    parklistEl.innerHTML = Arizona[i];
+    optionsList.appendChild(parklistEl);
+    }
+}
+if ( checkValue === "UTAH") { 
+
+    for (let i = 0; i < 5; i++) {
+
+    var Utah = [ "Arches National Park", "Bryce Canyon National Park", "Canyonlands National Park", "Capitol Reef National Park", "Zion National Park"];
+    var parklistEl = document.createElement("li");
+    parklistEl.className = "parksList";
+    parklistEl.innerHTML = Utah[i];
+    optionsList.appendChild(parklistEl);
+    }
+}
+ 
+if ( checkValue === "ALASKA") { 
+
+    for (let i = 0; i < 8; i++) {
+
+    var Alaska = ["Denali National Park","Gates of the Arctic National Park", "Glacier Bay National Park", "Katmai National Park", "Kenai Fjords National Park", "Kobuk Valley National Park", "Lake Clark National Park", "Wrangell St Elias National Park"];
+    var parklistEl = document.createElement("li");
+    parklistEl.className = "parksList";
+    parklistEl.innerHTML = Alaska[i];
+    optionsList.appendChild(parklistEl);
+    }
 }
 
- 
+if ( checkValue === "CALIFORNIA") { 
 
-
+    for (let i = 0; i < 8; i++) {
+    
+    var California = ["Channel Islands National Park", "Death Valley National Park", "Joshua Tree National Park", "Lassen Volcanic National Park", "Pinnacles National Park", "Redwood National Park", "Sequoia and Kings Canyon National Park", "Yosemite National Park"];  
+    var parklistEl = document.createElement("li");
+    parklistEl.className = "parksList";
+    parklistEl.innerHTML = California[i];
+    optionsList.appendChild(parklistEl);
+    }
+}
 
 
 
